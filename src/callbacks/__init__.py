1bbe8798361a7e3c9fb3c41db09b5ade5a20ea24
Fsoky <cyberuest0x12@gmail.com>
2024-05-17 20:55:03 +0500

    deleted moved or renamed

--- src/callbacks/__init__.py
+++
@@ -1,9 +0,0 @@
-from aiogram import Router
-
-
-def setup_callbacks_routers() -> Router:
-    from . import profile_settings
-
-    router = Router()
-    router.include_router(profile_settings.router)
-    return router
