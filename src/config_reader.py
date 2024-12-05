25068f403dc39da71c24501b9904cfdf036ccb00
DanIII ~ <61309653+Fsoky@users.noreply.github.com>
2024-12-06 01:34:41 +0500

    update config reader to match new variables and new .env path

--- src/config_reader.py
+++ src/config_reader.py
@@ -1,18 +1,19 @@
 from pathlib import Path
-from typing import Optional
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from pydantic import SecretStr
 
 ROOT_DIR = Path(__file__).parent.parent
-print(ROOT_DIR)
+
 
 class Config(BaseSettings):
     BOT_TOKEN: SecretStr
     DATABASE_URL: SecretStr
-    MONGO_INITDB_ROOT_USERNAME: Optional[str] = None
-    MONGO_INITDB_ROOT_PASSWORD: Optional[str] = None
-    ME_CONFIG_BASICAUTH_USERNAME: Optional[str] = None
-    ME_CONFIG_BASICAUTH_PASSWORD: Optional[str] = None
+
+    MONGO_INITDB_ROOT_USERNAME: str | None = None
+    MONGO_INITDB_ROOT_PASSWORD: str | None = None
+
+    ME_CONFIG_BASICAUTH_USERNAME: str | None = None
+    ME_CONFIG_BASICAUTH_PASSWORD: str | None = None
     
     model_config = SettingsConfigDict(
         env_file=ROOT_DIR / ".env",
