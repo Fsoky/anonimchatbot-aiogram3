92afcc107eec44b28a282989a974f5dd2c5b6c7b
Fsoky <cyberuest0x12@gmail.com>
2026-07-30 11:21:35 +0500

    Refactor main module to improve structure and integrate scene management

--- src/__main__.py
+++ src/__main__.py
@@ -1,34 +1,35 @@
-import asyncio
-
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
+from aiogram.fsm.scene import SceneRegistry
 from aiogram.enums import ParseMode
 
-from motor.motor_asyncio import AsyncIOMotorClient
+from src.bot.handlers import setup_routers, setup_scenes
+from src.bot.middlewares import UserMiddleware
 
-from handlers import setup_routers
-from middlewares import UserMiddleware
-
-from config_reader import config
+from src.core.config import config, translator_hub
 
 
 async def main() -> None:
+    config.set_project_level("dev")
+    
     bot = Bot(
         config.BOT_TOKEN.get_secret_value(),
         default=DefaultBotProperties(parse_mode=ParseMode.HTML)
     )
     dp = Dispatcher()
 
-    cluster = AsyncIOMotorClient(config.DATABASE_URL.get_secret_value())
-    db = cluster.anonimdb
-
     dp.message.middleware(UserMiddleware())
     dp.callback_query.middleware(UserMiddleware())
+    dp.edited_message.middleware(UserMiddleware())
     dp.include_router(setup_routers())
+    
+    registry = SceneRegistry(dp)
+    registry.add(*setup_scenes())
 
     await bot.delete_webhook(True)
-    await dp.start_polling(bot, db=db)
+    await dp.start_polling(bot, translator_hub=translator_hub)
 
 
 if __name__ == "__main__":
+    import asyncio
     asyncio.run(main())
